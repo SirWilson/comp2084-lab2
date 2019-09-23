30f976e6480bb9943c67de3b2874bc1e39809489
SirWilson <200369652@student.on.ca>
2019-09-23 11:16:20 -0400

    Add project files.

---
+++ COMP2084Lab2NoahWilson/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COMP2084Lab2NoahWilson.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(string category)
+        {
+            ViewBag.categories = category;
+            return View();
+        }
+    }
+}
